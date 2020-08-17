09a2574e775ae5b04a93e6fea789ca97bb491b72
Peter Williams <pedroeldiablo@yahoo.com>
2020-08-17 22:16:42 +0100

    wip checking if url is needed in new structure

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,13 +2,13 @@ import {IdentityContextProvider} from 'react-netlify-identity-widget';
 import React from 'react';
 
 export const wrapRootElement = ({element}) => {
-  const url = process.env.REACT_APP_NETLIFY_IDENTITY_URL || 'url here for running locally'; // should look something like "https://foo.netlify.com"
-  if (!url)
-    throw new Error(
-      'process.env.REACT_APP_NETLIFY_IDENTITY_URL is blank2, which means you probably forgot to set it in your Netlify environment variables'
-    );
+  // const url = process.env.REACT_APP_NETLIFY_IDENTITY_URL || 'url here for running locally'; // should look something like "https://foo.netlify.com"
+  // if (!url)
+  //   throw new Error(
+  //     'process.env.REACT_APP_NETLIFY_IDENTITY_URL is blank2, which means you probably forgot to set it in your Netlify environment variables'
+  //   );
   return (
-    <IdentityContextProvider url={url}>{url}{element}</IdentityContextProvider>
+    <IdentityContextProvider >{element}</IdentityContextProvider>
 
   );
 };
