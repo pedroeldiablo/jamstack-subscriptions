c4c391f24f9e626b2e8fe69ee1640713da3561b6
Peter Williams <pedroeldiablo@yahoo.com>
2020-08-17 21:56:57 +0100

    updating plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+module.exports = {plugins: [
+  // You can should only have one instance of this plugin
+  {
+    resolve: 'gatsby-plugin-netlify-identity',
+    options: {
+      url: 'https://jam-subscriptions.netlify.app/' // required!
+    }
+  }
+]};
