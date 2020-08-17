7065ff2114e6a3a6cdfe9f9dd74a5eb0206216cc
Peter Williams <pedroeldiablo@yahoo.com>
2020-08-17 22:37:47 +0100

    wip hardcoding url

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,13 +2,14 @@ import {IdentityContextProvider} from 'react-netlify-identity-widget';
 import React from 'react';
 
 export const wrapRootElement = ({element}) => {
+  const url =  'https://jam-subscriptions.netlify.app/';
   // const url = process.env.REACT_APP_NETLIFY_IDENTITY_URL || 'url here for running locally'; // should look something like "https://foo.netlify.com"
-  // if (!url)
-  //   throw new Error(
-  //     'process.env.REACT_APP_NETLIFY_IDENTITY_URL is blank2, which means you probably forgot to set it in your Netlify environment variables'
-  //   );
+  if (!url)
+    throw new Error(
+      'process.env.REACT_APP_NETLIFY_IDENTITY_URL is blank2, which means you probably forgot to set it in your Netlify environment variables'
+    );
   return (
-    <IdentityContextProvider >{element}</IdentityContextProvider>
+    <IdentityContextProvider url={url}>{element}</IdentityContextProvider>
 
   );
 };
