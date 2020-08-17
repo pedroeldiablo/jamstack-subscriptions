2a12044b006930c377d29f799aacfd02575f17a3
Peter Williams <pedroeldiablo@yahoo.com>
2020-08-17 20:45:03 +0100

    wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { IdentityModal, useIdentityContext } from 'react-netlify-identity-widget';
+import 'react-netlify-identity-widget/styles.css';
+import '@reach/tabs/styles.css';
 
 export const Index = () => {
-  return <p>TODO: Build a website</p>;
+  const identity = useIdentityContext();
+  const [dialog, setDialog] = useState(false);
+  const name =
+    (identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.full_name) || 'NoName';
+  return (
+    <>
+      { identity && identity.isLoggedIn && (
+        <pre>{JSON.stringify(identity, null, 2)}</pre>
+      )}
+      <h1>Sign up for Premium content</h1>
+      <p>Access exclusive posts and demos</p>
+      {!dialog && (<button onClick={() => setDialog(true)}>Log On</button>)}
+      <IdentityModal
+        showDialog={dialog}
+        onCloseDialog={() => setDialog(false)}
+        onLogin={(user) => console.log('hello ', user?.user_metadata)}
+        onSignup={(user) => console.log('welcome ', user?.user_metadata)}
+        onLogout={() => console.log('bye ', name)}
+      />
+
+    </>
+  );
 };
 
 export default Index;
