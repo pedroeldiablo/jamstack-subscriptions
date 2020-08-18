73b38cb47550e793edaf2381a0792095756548b1
Peter Williams <pedroeldiablo@yahoo.com>
2020-08-18 15:41:03 +0100

    WIP correcting return

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -4,6 +4,6 @@ exports.handler = async (event) => {
   
   return {
     statusCode: 200, 
-    app_metadata: {roles: ['sub:free']}
+    body: JSON.stringify({app_metadata: {roles: ['sub:free']}})
   };
 };
