1c21a6e5aeabf7253476572b7a6b7f122b0d731a
Peter Williams <pedroeldiablo@yahoo.com>
2020-08-20 00:12:13 +0100

    wip: typo

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -4,7 +4,7 @@ const { faunaFetch } = require('./utils/fauna');
 exports.handler = async (event, context) => {
   const { user } = context.clientContext;
   const {token} = context.clientContext.custom.identity;
-  const parts = access_token.split('.');
+  const parts = token.split('.');
   const currentUser = JSON.parse(atob(parts[1]));
 
   console.log({event});
