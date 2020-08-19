346305f9a4176c4961c4ce838e0370ee378ee67b
Peter Williams <pedroeldiablo@yahoo.com>
2020-08-19 01:48:30 +0100

    wip: reorder content and start to explore identity based content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,16 +10,17 @@ export const Index = () => {
     (identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.full_name) || 'NoName';
   return (
     <>
+      <h1>Sign up for Premium content</h1>
+      <p>Hi {name}</p>
+      <p>Access exclusive posts and demos</p>
       { identity && identity.isLoggedIn && (
         <pre>{JSON.stringify(identity, null, 2)}</pre>
       )}
-      <h1>Sign up for Premium content</h1>
-      <p>Access exclusive posts and demos</p>
       {!dialog && (<button onClick={() => setDialog(true)}>Log On</button>)}
       <IdentityModal
         showDialog={dialog}
         onCloseDialog={() => setDialog(false)}
-        onLogin={(user) => console.log('hello ', user?.user_metadata)}
+        onLogin={(user) => console.log('hello ', user?.user_metadata, user?.app_metadata)}
         onSignup={(user) => console.log('welcome ', user?.user_metadata)}
         onLogout={() => console.log('bye ', name)}
       />
