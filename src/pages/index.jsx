a41de3790c94b058a2db23883d1b7695e14d8b46
Peter Williams <pedroeldiablo@yahoo.com>
2020-08-19 20:31:45 +0100

    wip: ddisplaying content based on subscription

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { IdentityModal, useIdentityContext } from 'react-netlify-identity-widget';
 import 'react-netlify-identity-widget/styles.css';
 import '@reach/tabs/styles.css';
@@ -6,12 +6,28 @@ import '@reach/tabs/styles.css';
 export const Index = () => {
   const identity = useIdentityContext();
   const [dialog, setDialog] = useState(false);
+  const [isSubscribed, setIsSubscribed] = useState('not subscribed');
+
+  const loggedInEffect = () => {
+    if (identity && identity.user) {
+      const { roles } =  identity.user.app_metadata;
+      console.log({roles});
+      setIsSubscribed(roles);
+    } 
+  };
+         
+  useEffect(() => {
+    console.log('used loggedInEffect');
+    loggedInEffect();
+  });
+
   const name =
     (identity && identity.user && identity.user.user_metadata && identity.user.user_metadata.full_name) || 'NoName';
   return (
     <>
       <h1>Sign up for Premium content</h1>
       <p>Hi {name}</p>
+      <p>What's your subscription? {isSubscribed}</p>
       <p>Access exclusive posts and demos</p>
       { identity && identity.isLoggedIn && (
         <pre>{JSON.stringify(identity, null, 2)}</pre>
