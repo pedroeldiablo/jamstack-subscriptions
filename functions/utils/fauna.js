fb73f1eb0f81087840e82e6da5dd65f01159e464
Peter Williams <pedroeldiablo@yahoo.com>
2020-08-20 13:55:45 +0100

    wip: logging fauna arguements

--- functions/utils/fauna.js
+++ functions/utils/fauna.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 exports.faunaFetch = async({ query, variables}) => {
   console.log({query});
   console.log({variables});
+  console.log(process.env.FAUNA_SERVER_KEY);
 
   await fetch('https://graphql.fauna.com/graphql', {
     method: 'POST',
