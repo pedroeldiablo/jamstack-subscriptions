3a226caef6f785e987cc24373d1a51f006f107ea
Peter Williams <pedroeldiablo@yahoo.com>
2020-09-04 23:46:50 +0100

    WIP updating copy

---
+++ components/card/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Image from 'gatsby-image'
+
+export const Card = (props) => {
+  
+  <>
+  <div className="content">
+    <h2></h2>
+    <div className="free"></div>
+  </div>
+  </>
+}
